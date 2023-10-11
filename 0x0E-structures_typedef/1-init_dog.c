56f0464e364c1a4a348e35831b1f4ec9f99e0a75
Alexander-hash1 <alexctrimnell@gmail.com>
2023-10-11 16:47:15 -0700

    1-init_dog.c changes

--- 0x0E-structures_typedef/1-init_dog.c
+++ 0x0E-structures_typedef/1-init_dog.c
@@ -1,18 +1,13 @@
+#include <stdio.h>
 #include "dog.h"
-/**
- * init_dog - check the code for Holberton School students.
- * @d: pointer
- * @name: naming
- * @age: number
- * @owner: pointer
- * Return: Always 0.
- */
-void init_dog(struct dog *d, char *name, float age, char *owner)
+
+int main(void)
 {
-if (d)
-{
-d->name = name;
-d->age = age;
-d->owner = owner;
+    struct dog my_dog;
+
+    init_dog(&my_dog, "Poppy", 3.5, "Bob");
+    printf("My name is %s, and I am %.1f :) - Woof!\n", my_dog.name, my_dog.age);
+
+    return (0);
 }
 }
